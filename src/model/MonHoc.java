b8a197a376a704d415b75b372f594fbb1a10c28c
hnam2802 <h2so4zzzz@.com>
2025-11-17 22:33:11 +0700

    Mô tả ngắn gọn công việc đã làm (ví dụ: Fix bug dang nhap)

--- src/model/MonHoc.java
+++ src/model/MonHoc.java
@@ -9,5 +9,41 @@ package model;
  * @author Admin
  */
 public class MonHoc {
-    
+    private static int tmp = 1;
+    private String maMonHoc, tenMon, loaiMonHoc;
+    private int soTiet;
+    public MonHoc() {
+        
+    }
+    public MonHoc(String ten, String loai, int sl) {
+        this.maMonHoc = String.format("%03d", tmp++);
+        this.tenMon = ten;
+        this.loaiMonHoc = loai;
+        this.soTiet = sl;
+    }
+    public void getTenMonHoc(String name) {
+        this.tenMon = name;
+    }
+    public void getLoaiMonHoc(String type) {
+        this.loaiMonHoc = type;
+    }
+    public void getSoTiet(int sl) {
+        this.soTiet = sl;
+    }
+    public String getTenMonHoc() {
+        return tenMon;
+    }
+    public String getMaMonHoc() {
+        return maMonHoc;
+    }
+    public String getLoaiMonHoc() {
+        return loaiMonHoc;
+    }
+    public int getSoTiet() {
+        return soTiet;
+    }
+    @Override 
+    public String toString() {
+        return getMaMonHoc() + " " + getTenMonHoc() + " " + getSoTiet() + " " + getLoaiMonHoc();
+    } 
 }
